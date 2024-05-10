d930b16d79b85a0064466307ca109b5f3006ad83
lkkrd <leshkord@gmail.com>
2024-05-11 00:31:23 +0200

    scraping pierwszej strony

---
+++ hello-scraper/Offer.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V122.DOM;
+using Json.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace hello_scraper
+{
+    internal class Offer
+    {
+        public string Xpath;
+        public string Major;
+        public string Date;
+        public string Location;
+        [JsonIgnore]
+        public IWebElement ParentElement;
+        public Offer(IWebElement parentElement)
+        {
+            this.ParentElement = parentElement;
+            this.Major = ParentElement.FindElement(By.ClassName("spec")).Text;
+            this.Location = ParentElement.FindElement(By.ClassName("workplace")).Text;
+            this.Date = ParentElement.FindElement(By.ClassName("time-ago")).GetAttribute("data-time-ago");
+        }
+
+        public string Desc()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ hello-scraper/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V122.CSS;
+
+namespace hello_scraper;
+
+class Program
+{
+    public static void Main()
+    {
+        var driver = new ChromeDriver();
+        var offerList = new List<Offer>();
+        int offerIterator = 1;
+        string xpath = "//*[@id=\"custom-list\"]/li[" + Convert.ToString(offerIterator) + "]";
+
+        var majorDict = new Dictionary<string, int>();
+        var locationDict = new Dictionary<string, int>();
+    
+        driver.Url = "https://konsylium24.pl/kompendium24/praca/oferty-pracy/lista/";
+
+        var offerIWebElementList = driver.FindElements(By.ClassName("list-group-offer"));
+
+        foreach(IWebElement element in offerIWebElementList)
+        {
+            Offer o = new Offer(element);
+            offerList.Add(o);
+
+            if (!majorDict.TryGetValue(o.Major, out int _))
+            {
+                majorDict.Add(o.Major, 0);
+            }
+            majorDict[o.Major]++;
+        }
+    }
+
+    public Offer getOffers(ChromeDriver driver)
+    {
+        
+        throw new NotImplementedException();
+    }
+}
